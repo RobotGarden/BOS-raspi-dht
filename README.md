1d232d12f358ae5618ac240d1636faefe6d414f0
Daniel Casner <daniel@anki.com>
2016-06-08 09:33:04 -0700

    Concept for restructuring to use MQTT, lots of work still to do.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-ROS BOS digital temperature and humidity sensor driver for raspberri pi
-License: BSD
+# BOS RASPI DHT
+
+Pure C++ program to read a DHT sensor and return results over the network as part of a home automation system.
+
+## MQTT Version
+This branch contains a version which has been written to broadcast results over MQTT.
+
+## History
+This program is based on a DHT sensor reader published by Adafruit industries which I rewrote to publish over ROS. It has now been rewritten again to publish on MQTT instead because the existing state of ROS 1.x is too cumbersome. 
--- src/bos_raspi_dht_node.cpp
+++ src/bos_raspi_dht_node.cpp
@@ -26,9 +26,9 @@
 #include <sys/time.h>
 #include <bcm2835.h>
 #include <unistd.h>
+#include <std::string>
 
-#include "ros/ros.h"
-#include "std_msgs/Float32.h"
+#include <mosquittopp.h>
 
 #define MAXTIMINGS 100
 
@@ -38,6 +38,142 @@
 #define DHT22 22
 #define AM2302 22
 
+class BosDhtNode : public mosqpp::mosquittopp
+{
+public:
+  BosDhtNode(const std::string& topicStem, const int sampleIntervalSeconds=30, const char* id=NULL, const char* host="127.0.0.1", const int port=1883, const int keepAlive=120);
+  ~BosDhtNode();
+  
+  void on_connect(int rc);
+  void on_disconnect(int rc);
+  void on_publish(int mid);
+  
+  int run();
+  
+  typedef enum {
+    initalized,
+    connected,
+    failedToConnect,
+    disconnected,
+    sampling,
+    publishTmp,
+    publishHum,
+    waiting,
+  } BDNState;
+
+private:
+  void publishSample(const std::string& topic, const float sample);
+  
+  BDNState state;
+  const int interval;
+  int msgCount;
+  const std::string temperatureTopic;
+  const std::string humidityTopic;
+  float temperatureSample;
+  float humiditySample;
+  struct timeval lastSampleTime;
+};
+
+BosDhtNode::BosDhtNode(const std::string& topicStem, const int sampleIntervalSeconds, const char* id, const char* host, const int port, const int keepAlive) :
+  mosquittopp(id),
+  state(initalized),
+  interval(sampleIntervalSeconds),
+  msgCount(0),
+  temperatureTopic(topicStem + std::string("/temperature")),
+  humidityTopic(topicStemp + std::string("/humidity")) {
+  mosqpp::lib_init();
+  connect_async(host, port, keepAlive);
+  //loop_start();
+}
+
+BosDhtNode::~BosDhtNode() {
+  //loop_stop();
+  mosqpp::lib_cleanup();
+}
+
+void BosDhtNode::on_connect(int rc)
+{
+  if (rc == 0) {
+    printf("Connected to broker\r\n");
+    state = connected;
+  }
+  else {
+    printf("Couldn't connect to broker: %d\r\n", rc);
+    state = failedToConnect;
+  }
+}
+
+void BosDhtNode::on_disconnect(int rc)
+{
+  state = disconnected;
+  printf("Disconnected from broker: %d\r\n", rc);
+}
+
+void BosDhtNode::on_publish(int mid)
+{
+  if (mid != msgCount) {
+    printf("Unexpected published callback for mid %d while in state %d\r\n", mid, state);
+  }
+  else if (state == publishTmp) {
+    state = publishHum;
+    msgCount++;
+    publishSample(humidityTopic, humiditySample);
+  }
+  else if (state == publishHum) {
+    state = waiting;
+    msgCount++;
+  }
+  else {
+    printf("Unexpected state when receiving published callback: %d, mid=%d\r\n", state, mid);
+    state = waiting;
+    msgCount++;
+  }
+}
+
+int BosDhtNode::run()
+{
+  while (true) {
+    loop();
+    switch (state) {
+      case initalized:
+        continue;
+      case connected:
+        state = sampling;
+        continue;
+      case failedToConnect:
+      case disconnected:
+        return -1;
+      case sampling:
+      {
+        XXX Sample the sensor and publish the result
+        temperatureSample = XXX
+        humiditySample = XXX
+        gettimeofday(&lastSampleTime, NULL);
+        state = publishTmp;
+        publishSample(temperatureTopic, temperatureSample);
+        continue;
+      }
+      case publishTmp:
+      case publishHum:
+        continue;
+      case waiting:
+      {
+        struct timeval now;
+        gettimeofday(&now, NULL);
+        if (now.tv_sec > (lastSampleTime.tv_sec + interval)) state = sampling;
+        continue;
+      }
+    }
+  }
+}
+
+void BosDhtNode::publishSample(const std::string& topic, const float sample)
+{
+  const size_t maxDataLen = 1024;
+  char jsonData[maxDataLen];
+  int jsonLen = snprintnf(jsonData, maxDataLen, "%f", sample);
+  publish(&msgCount, topic.c_str(), jsonLen, jsonData, 0, true);
+}
 
 int readDHT(int type, int pin, float* t, float* h);
 
@@ -50,6 +186,8 @@ int main(int argc, char **argv) {
     printf("example: %s 2302 4 - Read from an AM2302 connected to GPIO #4\n", argv[0]);
     return 2;
   }
+
+  
 
   int type = 0;
   if (strcmp(argv[1], "11") == 0) type = DHT11;
