e73c27a4faf1684e0c20a4a4efe4fc37c60ffd52
Daniel Casner <daniel@anki.com>
2016-09-21 16:21:59 -0700

    Working on main. Need to import new DHT library.

--- src/bos_raspi_dht_node.cpp
+++ src/bos_raspi_dht_node.cpp
@@ -41,7 +41,7 @@
 class BosDhtNode : public mosqpp::mosquittopp
 {
 public:
-  BosDhtNode(const std::string& topicStem, const int sampleIntervalSeconds=30, const char* id=NULL, const char* host="127.0.0.1", const int port=1883, const int keepAlive=120);
+  BosDhtNode(const int sensorType, const int dhtPin, const std::string& topicStem, const int sampleIntervalSeconds=30, const char* id=NULL, const char* host="127.0.0.1", const int port=1883, const int keepAlive=120);
   ~BosDhtNode();
   
   void on_connect(int rc);
@@ -63,9 +63,12 @@ public:
 
 private:
   void publishSample(const std::string& topic, const float sample);
+  int  readDHT();
   
   BDNState state;
-  const int interval;
+  const int sensorType;
+  const int type;
+  const int pin;
   int msgCount;
   const std::string temperatureTopic;
   const std::string humidityTopic;
@@ -74,9 +77,11 @@ private:
   struct timeval lastSampleTime;
 };
 
-BosDhtNode::BosDhtNode(const std::string& topicStem, const int sampleIntervalSeconds, const char* id, const char* host, const int port, const int keepAlive) :
+BosDhtNode::BosDhtNode(const int sensorType, const int dhtPin, const std::string& topicStem, const int sampleIntervalSeconds, const char* id, const char* host, const int port, const int keepAlive) :
   mosquittopp(id),
   state(initalized),
+  type(sensorType),
+  pin(dhtPin),
   interval(sampleIntervalSeconds),
   msgCount(0),
   temperatureTopic(topicStem + std::string("/temperature")),
@@ -145,12 +150,11 @@ int BosDhtNode::run()
         return -1;
       case sampling:
       {
-        XXX Sample the sensor and publish the result
-        temperatureSample = XXX
-        humiditySample = XXX
-        gettimeofday(&lastSampleTime, NULL);
-        state = publishTmp;
-        publishSample(temperatureTopic, temperatureSample);
+        if (readDHT()) {
+          gettimeofday(&lastSampleTime, NULL);
+          state = publishTmp;
+          publishSample(temperatureTopic, temperatureSample);
+        }
         continue;
       }
       case publishTmp:
@@ -175,11 +179,26 @@ void BosDhtNode::publishSample(const std::string& topic, const float sample)
   publish(&msgCount, topic.c_str(), jsonLen, jsonData, 0, true);
 }
 
-int readDHT(int type, int pin, float* t, float* h);
+int BosDhtNode::readDHT() {
+  XXX Reimport adafruit library
+}
+
 
 int main(int argc, char **argv) {
 
   if (!bcm2835_init()) return 1;
+
+  int sensorType = -1;
+  int dhtPin = -1;
+  char* topicStem = "dht";
+  int sampleInterval = 30;
+  char* id = argv[0];
+  char* host = "127.0.0.1";
+  int port = 1883;
+  int keepAlive = 120;
+  
+
+
 
   if (argc < 3) {
     printf("usage: %s {11|22|2302} GPIOpin# [ROS Parameters]\n", argv[0]);
@@ -206,131 +225,8 @@ int main(int argc, char **argv) {
 
   printf("Using pin #%d\n", dhtpin);
 
-  ros::init(argc, argv, "raspi_dht");
-
-  ros::NodeHandle n;
-
-  ros::Publisher temperature_pub = n.advertise<std_msgs::Float32>("temperature", 2);
-  ros::Publisher humidity_pub    = n.advertise<std_msgs::Float32>("humidity", 2);
-
-  ros::Rate loop_rate(0.016);
-  ros::Duration retry_interval(5.0);
-  ros::Duration backoff_interval(200);
-
-  bool last_had_data = true;
-
-  while (ros::ok()) {
-    std_msgs::Float32 temperature_msg, humidity_msg;
-    float t, h;
-
-    if (readDHT(type, dhtpin, &t, &h)) {
-      temperature_msg.data = t;
-      humidity_msg.data = h;
-#ifdef DEBUG
-      printf("Publishing data: t=%f, h=%f\n", t, h);
-      ROS_INFO("BOS_RASPI_DHT_NODE: %f, %f", t, h);
-#endif
-      temperature_pub.publish(temperature_msg);
-      humidity_pub.publish(humidity_msg);
-      ros::spinOnce();
-      loop_rate.sleep();
-      last_had_data = true;
-    }
-    else {
-      if (last_had_data) {
-	ROS_INFO("BOS_raspi_DHT_node: no data, retrying soon");
-	retry_interval.sleep();
-      }
-      else {
-	ROS_INFO("BOS_raspi_DHT_node: no data, waiting a while");
-	backoff_interval.sleep();
-      }
-      last_had_data = false;
-    }
   }
 
   return 0;
 
 } // main
-
-
-int readDHT(int type, int pin, float* t, float* h) {
-  int bits[250], data[100];
-  int bitidx = 0;
-  int counter = 0;
-  int laststate = HIGH;
-  int j=0;
-
-  // Set GPIO pin to output
-  bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP);
-
-  bcm2835_gpio_write(pin, HIGH);
-  usleep(500000);  // 500 ms
-  bcm2835_gpio_write(pin, LOW);
-  usleep(20000);
-
-  bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_INPT);
-
-  data[0] = data[1] = data[2] = data[3] = data[4] = 0;
-
-  // wait for pin to drop?
-  while (bcm2835_gpio_lev(pin) == 1) {
-    usleep(1);
-  }
-
-  // read data!
-  for (int i=0; i< MAXTIMINGS; i++) {
-    counter = 0;
-    while ( bcm2835_gpio_lev(pin) == laststate) {
-	counter++;
-	//nanosleep(1);		// overclocking might change this?
-        if (counter == 1000)
-	  break;
-    }
-    laststate = bcm2835_gpio_lev(pin);
-    if (counter == 1000) break;
-    bits[bitidx++] = counter;
-
-    if ((i>3) && (i%2 == 0)) {
-      // shove each bit into the storage bytes
-      data[j/8] <<= 1;
-      if (counter > 200)
-        data[j/8] |= 1;
-      j++;
-    }
-  }
-
-
-#ifdef DEBUG
-  for (int i=3; i<bitidx; i+=2) {
-    printf("bit %d: %d\n", i-3, bits[i]);
-    printf("bit %d: %d (%d)\n", i-2, bits[i+1], bits[i+1] > 200);
-  }
-#endif
-
-  if ((j >= 39) &&
-      (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) ) {
-    // yay!
-    if (type == DHT11) {
-      *t = (float)data[2];
-      *h = (float)data[0];
-#ifdef DEBUG
-      printf("Temp = %d *C, Hum = %d \%\n", data[2], data[0]);
-#endif
-    }
-     if (type == DHT22) {
-	*h = data[0] * 256 + data[1];
-	*h /= 10;
-
-	*t = (data[2] & 0x7F)* 256 + data[3];
-        *t /= 10.0;
-        if (data[2] & 0x80) *t *= -1;
-#ifdef DEBUG
-	printf("Temp =  %.1f *C, Hum = %.1f \%\n", *t, *h);
-#endif
-    }
-    return 1;
-  }
-
-  return 0;
-}
